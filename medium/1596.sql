794cfcac32660e83bd11f4fa97d8f407f4ff0a32
Meng <82090573+menghsuanl@users.noreply.github.com>
2023-04-24 14:05:01 -0700

    Update 1596.sql

--- medium/1596.sql
+++ medium/1596.sql
@@ -38,3 +38,19 @@ FROM
 LEFT JOIN Products p
 ON sub3.product_id = p.product_id
 ORDER BY 1,2
+
+
+
+/* More concise sol*/
+SELECT customer_id, product_id, product_name
+FROM (
+    SELECT O.customer_id, O.product_id, P.product_name, 
+    RANK() OVER (PARTITION BY customer_id ORDER BY COUNT(O.product_id) DESC) AS rnk
+    FROM Orders O
+    JOIN Products P
+    ON O.product_id = P.product_id  
+    GROUP BY customer_id, product_id
+) temp
+WHERE rnk = 1 
+ORDER BY customer_id, product_id
+
