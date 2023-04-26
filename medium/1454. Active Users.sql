df74fbde5895c48ca6d07ff7aee8a1e7f6b041d1
Meng <82090573+menghsuanl@users.noreply.github.com>
2023-04-26 15:51:04 -0700

    Update 1454. Active Users.sql

--- medium/1454. Active Users.sql
+++ medium/1454. Active Users.sql
@@ -1,3 +1,16 @@
+/* self join and datediff between 1 to 4 */
+WITH temp AS (SELECT l1.id, l1.login_date, COUNT(DISTINCT l2.login_date) AS cnt
+FROM Logins l1
+LEFT JOIN Logins l2 ON l1.id = l2.id AND DATEDIFF(l2.login_date, l1.login_date) BETWEEN 1 AND 4
+GROUP BY 1,2
+HAVING cnt>=4)
+
+SELECT DISTINCT temp.id, name
+FROM temp
+JOIN Accounts ON temp.id = Accounts.id
+ORDER BY 1;
+
+
 /* group and island */
 with temp0 AS
 (SELECT  id,
