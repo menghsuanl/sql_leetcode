91d5836e2b41f52b30209ed33d2db81adddd716c
Meng <82090573+menghsuanl@users.noreply.github.com>
2023-05-03 15:52:35 -0700

    Create 602. Friend Requests II: Who Has the Most Friends.sql

---
+++ medium/602. Friend Requests II: Who Has the Most Friends.sql
@@ -0,0 +1,16 @@
+SELECT
+id,
+COUNT(*) AS num
+FROM
+(
+SELECT
+requester_id AS id
+FROM RequestAccepted
+UNION ALL
+SELECT 
+accepter_id AS id
+FROM RequestAccepted
+)sub
+GROUP BY id
+ORDER BY COUNT(*) DESC
+LIMIT 1
